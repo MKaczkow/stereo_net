8377c10c3118433b483a37cf749959f373a06c9b
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2023-08-20 18:22:29 +0200

    EOD

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,40 +1,61 @@
 import gradio as gr
 import torch
-from PIL import Image
+from torchvision.transforms import ToTensor
 import io
-
-# NOTE: launch from jupyter notebook, otherwise it will not work
-
 # Load your PyTorch model
 
 if torch.cuda.is_available():
-    model = torch.load("./src/ui/best-model.ckpt")
+    model = torch.load("./best-model.ckpt")
+    # model = torch.load("./src/ui/best-model.ckpt")
 else:
-    model = torch.load("./src/ui/best-model.ckpt", map_location=torch.device('cpu'))
+    model = torch.load("./best-model.ckpt", map_location=torch.device('cpu'))
+    # model = torch.load("./src/ui/best-model.ckpt", map_location=torch.device('cpu'))
 # model = torch.load("../model/BEST-epoch=23-step=864006.ckpt")
 # model.eval()
+class Rescale():
+    """
+    Rescales the left and right image tensors (initially ranged between [0, 1]) and rescales them to be between [-1, 1].
+    """
+
+    @staticmethod
+    def __call__(sample):
+        for name in ['left', 'right']:
+            sample[name] = (sample[name] - 0.5) * 2
+        return sample
+    
+# def png_loader(path):
+#     img = io.imread(path)
+#     return img
 
 # Function to convert PNG to WebP
-def convert_to_webp(png_image):
-    png_image = Image.open(io.BytesIO(png_image))
-    webp_image = png_image.convert("RGB").save(io.BytesIO(), "WEBP")
-    return webp_image.getvalue()
+def prepare_sample(left_image, right_image):
 
-# Function to perform disparity estimation
+    # left_image = png_loader(left_image)
+    # right_imate = png_loader(right_imate)
+
+    # transforms = [ToTensor(), Rescale()]
+    transforms = [ToTensor()]
+    # torch_sample = ToTensor()(sample)
+    for transform in transforms:
+        left_image = transform(left_image)
+        right_image = transform(right_image)
+
+    sample = {
+        'left': left_image, 'right': right_image, 
+        }
+
+    return sample
+
 def disparity_estimation(left_image, right_image):
-    # Convert PNG to WebP
-    left_webp = convert_to_webp(left_image)
-    right_webp = convert_to_webp(right_image)
 
-    # Process the images and perform disparity estimation with your model
-    # You'll need to adjust this part according to your model's specific requirements
-    disparity_map = model(left_webp, right_webp)
-
-    # Return the disparity map as a PNG image
+    sample = prepare_sample(left_image, right_image)
+    # disparity_map = model(sample['left'], sample['right'])
+    with torch.no_grad():
+        disparity_map = model(sample)
     return disparity_map
 
 # Create a Gradio interface
-iface = gr.Interface(
+demo = gr.Interface(
     fn=disparity_estimation,
     inputs=[
         gr.inputs.Image(type="pil", label="Left Image (PNG)"),
@@ -44,6 +65,6 @@ iface = gr.Interface(
     title="Disparity Estimation",
     description="Upload two PNG images for left and right views to estimate disparity.",
 )
-
 # Start the Gradio UI
-iface.launch()
+demo.launch()
+
